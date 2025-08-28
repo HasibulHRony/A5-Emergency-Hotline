27a3dcdd91456ac906c19922e3ed5df304596894
HasibulHRony <hasibulhrony7@gmail.com>
2025-08-28 09:24:14 +0600

    first commit

---
+++ script/script.js
@@ -0,0 +1,66 @@
+
+
+//function of getting element by id
+function gettingElementBYId(id) {
+    const element = document.getElementById(id);
+    return element;
+}
+
+gettingElementBYId("all-hotlines").addEventListener("click", function (e) {
+
+    //heart count increasing functionality
+    if (e.target.className.includes("heart") === true) {
+        let heartCountText = gettingElementBYId("heart-count").innerText;
+        let heartCountNumber = Number(heartCountText);
+        heartCountNumber++;
+        gettingElementBYId("heart-count").innerText = heartCountNumber;
+    }
+
+    //call buttons functionality
+    if (e.target.className.includes("call-btn") === true) {
+        // getting the title and number and img from cart
+        const title = e.target.parentNode.parentNode.children[1].children[0].innerText;
+        const hotlineNumber = e.target.parentNode.parentNode.children[2].children[0].innerText;
+
+        //getting the coin number
+        let coins = gettingElementBYId("coins").innerText;
+        let coinsNumber = Number(coins);
+
+
+        // sawing alert for calling
+        if (coinsNumber < 20) {
+            // sawing alert not sufficient coins for calling 
+            return alert("You Don't Have Sufficient Coin. Try again later.");
+        }
+        else {
+            //sawing alert for call and minus coins
+            alert(`You are calling at ${title}. Hotline is ${hotlineNumber}. Are you confirm?`);
+            coinsNumber = coinsNumber - 20;
+            gettingElementBYId("coins").innerText = coinsNumber;
+        }
+
+        const now = new Date();
+        const currentTime = now.toLocaleTimeString();
+        const div = document.createElement("div");
+        div.innerHTML = `
+            <div class="flex justify-between items-center my-2 p-4 bg-white rounded-lg shadow-md">
+                <div class="flex flex-col">
+                    <h3 class="text-lg font-semibold mb-2">${title}</h3>
+                    <p>${hotlineNumber}</p>
+                </div>
+                <div class="flex items-center">
+                    ${currentTime}    
+                </div>
+            </div> 
+            `;
+        const historyContainer = gettingElementBYId("history-container");
+        historyContainer.appendChild(div);
+
+    }
+})
+
+// clear btn functionality
+gettingElementBYId("clear-btn").addEventListener("click", function(){
+    const clearHistory = gettingElementBYId("history-container");
+    clearHistory.innerHTML = "";
+})
